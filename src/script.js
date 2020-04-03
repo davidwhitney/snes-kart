82b05b8af117209e0d5d7fefb9d1e1f1e13c8bc6
Glitch (trainagotchi) <none>
2020-04-03 14:21:42 +0000

    💸🐿 Checkpoint ./README.md:7701387/9975 ./src/Config.js:7701387/1706 ./src/Game.js:7701387/3208 ./src/GameUi.js:7701387/5717 ./src/script.js:7701387/478 ./views/index.html:7701387/23650 ./server.js:7701387/493 ./package.json:7701387/22 ./public/style.css:7701387/11451 ./public/script.js:7701387/6224

--- src/script.js
+++ src/script.js
@@ -1,24 +1,11 @@
 const Game = require("./Game");
 const GameUi = require("./GameUi");
-
-const AblyTrainArrivalsClient = require("./AblyTrainArrivalsClient");
-const SimulatedTrainArrivalsClient = require("./SimulatedTrainArrivalsClient");
-
-let game, ui, dataSource;
+let game, ui;
 
 async function startGame(useRealData = false) {
-  dataSource = useRealData 
-                ? new AblyTrainArrivalsClient() 
-                : new SimulatedTrainArrivalsClient();
-  
   game = new Game();
-  ui = new GameUi(game);
-  
-  game.start({
-    onGameStart: async () => await dataSource.listenForEvents("northern:940GZZLUKSX", msg => game.registerEvent(game, msg)),
-    onGameEnd: () => dataSource.stopListening()
-  });
-
+  ui = new GameUi(game);  
+  game.start();
   ui.startRendering(game);
   
   return game;
